7a6c8491a7af5aa3c03c819dfaaf183dfc071adf
A <alexey.grishan@gmail.com>
2024-01-28 13:04:54 +0100

    fix gpu selection

--- crates/bizarre_render/src/vulkan/device.rs
+++ crates/bizarre_render/src/vulkan/device.rs
@@ -1,7 +1,11 @@
-use std::ops::{Deref, DerefMut};
+use std::{
+    ffi::CStr,
+    ops::{Deref, DerefMut},
+};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use ash::{extensions::khr, vk};
+use bizarre_logger::core_info;
 
 use super::instance::VulkanInstance;
 
@@ -21,10 +25,32 @@ impl VulkanDevice {
 
         let surface_loader = khr::Surface::new(&instance.entry, instance);
 
-        let (pdevice, queue_family_index) = pdevices
+        let pdevice_props = {
+            pdevices
+                .iter()
+                .map(|p| instance.get_physical_device_properties(*p))
+                .collect::<Vec<_>>()
+        };
+
+        let pdevice_names = pdevice_props
             .iter()
-            .find_map(|p| {
-                instance
+            .map(|p| {
+                let name = CStr::from_ptr(p.device_name.as_ptr()).to_str()?.to_string();
+                Ok(name)
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        let message = pdevice_names
+            .iter()
+            .fold("".to_string(), |acc, n| format!("{}\n\t{}", acc, n));
+        core_info!("Available GPUs:{}", message);
+
+        let mut pdevices_map = pdevices
+            .iter()
+            .zip(pdevice_names)
+            .zip(pdevice_props)
+            .map(|((p, n), props)| {
+                let queue_index = instance
                     .get_physical_device_queue_family_properties(*p)
                     .iter()
                     .enumerate()
@@ -36,15 +62,47 @@ impl VulkanDevice {
                                     .unwrap();
 
                         if supports_graphics_and_surface {
-                            Some((*p, i))
+                            Some(i)
                         } else {
                             None
                         }
-                    })
-            })
-            .expect("Failed to find suitable device.");
+                    });
+                if queue_index.is_none() {
+                    return (*p, 0, 0, n);
+                }
 
-        let queue_family_index = queue_family_index as u32;
+                let mut rating = 0;
+
+                let memory_props = instance.get_physical_device_memory_properties(*p);
+
+                rating += memory_props
+                    .memory_heaps
+                    .iter()
+                    .map(|m| m.size)
+                    .reduce(|acc, e| acc + e)
+                    .unwrap_or(0);
+
+                rating *= match props.device_type {
+                    vk::PhysicalDeviceType::DISCRETE_GPU => 1000,
+                    vk::PhysicalDeviceType::INTEGRATED_GPU => 100,
+                    _ => 1,
+                };
+
+                (*p, rating, queue_index.unwrap(), n)
+            })
+            .collect::<Vec<_>>();
+
+        pdevices_map.sort_by(|a, b| a.1.cmp(&b.1).reverse());
+
+        if (pdevices_map.first().is_none() || pdevices_map.first().unwrap().1 <= 0) {
+            bail!("Failed to find suitable physical device");
+        }
+
+        let (pdevice, _, queue_family_index, pdevice_name) = pdevices_map.first().unwrap();
+
+        core_info!("Selected GPU: {}", pdevice_name);
+
+        let queue_family_index = *queue_family_index as u32;
         let device_extension_names_raw = [khr::Swapchain::name().as_ptr()];
         let pdevice_features = vk::PhysicalDeviceFeatures::builder()
             .shader_clip_distance(true)
@@ -60,7 +118,7 @@ impl VulkanDevice {
             .enabled_features(&pdevice_features);
 
         let device = instance
-            .create_device(pdevice, &device_create_info, None)
+            .create_device(*pdevice, &device_create_info, None)
             .expect("Failed to create a Vulkan Device");
 
         let graphics_queue = device.get_device_queue(queue_family_index, 0);
@@ -68,7 +126,7 @@ impl VulkanDevice {
 
         Ok(Self {
             handle: device,
-            physical_device: pdevice,
+            physical_device: *pdevice,
             graphics_queue,
             present_queue,
             queue_family_index,
